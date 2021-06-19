a4854acc9a2f1a8ee8fdae16d9a2da6989514e23
Nicolas Vauché <hello@nicolasvauche.net>
2021-06-19 15:53:19 +0200

    Contact processing

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -19,15 +20,38 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact/{email}", name="contact")
      */
-    public function contact(): Response
+    public function contact(Request $request, $email = null): Response
     {
-        $contactData = new Contact();
+        $editmode = false;
+
+        if ($email) {
+            $contactData = $this->getDoctrine()->getRepository(Contact::class)->findOneBy(['email' => $email]);
+            if ($contactData) {
+                $editmode = true;
+            }
+        } else {
+            $contactData = new Contact();
+        }
+
         $form = $this->createForm(ContactType::class, $contactData);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactData = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contactData);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre message a été enregistré !');
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('default/contact.html.twig', [
             'form' => $form->createView(),
+            'editmode' => $editmode,
         ]);
     }
 }
